9ed29c7c2cd2924c7c7aea4b6e2a3455101b679b
Peyton Seigo <pseigo@yahoo.com>
2017-05-07 02:27:26 -0600

    more descriptive function name, comment cleanup

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 void fillVector(vector<pair<int, bool>> &number, int n); // Fill vector with numbers up to n
 void setPrimes(vector<pair<int, bool>> &prime, int maxNumber); // Set all primes in vector to 'true' and composites to 'false'
-void printPrimes(vector<pair<int, bool>> const &prime); // Print primes by a const reference (can't change value)
+void printPrimes(vector<pair<int, bool>> const &prime);
 
 void printSyntaxError();
-void runPrime(int n); // executes prime functions with n
+void getPrimesUpTo(int n);
 int askForNumber();
 
 
@@ -19,24 +19,24 @@ int main(int argc, char *argv[])
     // Decide how to execute program based on command line arguments (or a lack of)
     if (argc == 1) // menu interface
     {
-        runPrime(askForNumber());
+        getPrimesUpTo(askForNumber());
         cout << endl;
     } else if (argc == 2) // given a single argument
     {
         /*
-         * int 'argument' is initialized to -1. If atoi() is unable to find a number
-         * from argv[1], 'argument' will remain as -1. We can utilize this to tell
+         * int 'argument' is initialized to 0. If atoi() is unable to find a number
+         * from argv[1], 'argument' will remain as 0. We can utilize this to tell
          * when the user gives an invalid number (anything less than 1 or no number
          * at all).
         */
 
         int argument = 0;
-        argument = atoi(argv[1]);
+        argument = atoi(argv[1]); // convert any numbers (as a string) from the argument to an int
 
         if (argument < 1 || argument > 10000000) {
             printSyntaxError(); return 1;
         } else {
-            runPrime(argument);
+            getPrimesUpTo(argument);
         }
     }
     else if (argc > 1) // syntax error, every other case
@@ -61,31 +61,24 @@ void fillVector(vector<pair<int, bool>> &number, int n)
 // Set all primes in vector to 'true' and composites to 'false'
 void setPrimes(vector<pair<int, bool>> &prime, int maxNumber)
 {
-    // process to mark non-primes (composite numbers) as false
+    // mark non-primes (composite numbers) as false
     for (int current = 2; current <= sqrt(maxNumber); current++) {
-
         // if current number is true, theorem states it is PRIME!
         if (prime[current - 1].second == true) {
-
-            // starting at the square of the current number, go through every number
-            // up to maxNumber and if it's a factor of current, mark it as true.
             for (int factor = (current*current); factor <= maxNumber; factor++) {
+                // if the current number is a factor, number is composite
                 if (factor % current == 0) {
-                    // the factor of the current number is composite, therefore set to false
                     prime[factor - 1].second = false;
                 }
             }
-
         } // end of prime condition
     }
 
     return;
 }
 
-// Print primes by a const reference (can't change value)
 void printPrimes(vector<pair<int, bool>> const &prime)
 {
-    // print all prime numbers (prime has boolean 'true')
     for (pair<int, bool> current : prime) {
         if (current.second == true)
             cout << current.first << ' ';
@@ -95,15 +88,13 @@ void printPrimes(vector<pair<int, bool>> const &prime)
 }
 
 /* ============== Interface Functions ============== */
-// informs user of syntax
 void printSyntaxError()
 {
     cerr << "Usage: primesto.exe [1 ... 10000000] \n"
         << "\tCalculates all prime numbers up to a given number." << endl;
 }
 
-// finds primes up to parameter n
-void runPrime(int n)
+void getPrimesUpTo(int n)
 {
     if (n > 0) {
         // int refers to number, bool refers to primeness (if true, its prime)
@@ -126,7 +117,6 @@ int askForNumber()
     do {
         cout << "Enter a positive number: ";
         cin >> number;
-        // number = atoi(input);
 
         if (!cin.good() || number < 1 || number > 10000000) {
             cout << "ERROR: Must be a number and greater than 0! \n" << endl;
